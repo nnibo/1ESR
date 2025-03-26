bbc9e77f9c8d459c1fcc15b30cb69a4e75760336
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 22:05:40 -0300

    exercicios feitos em python

---
+++ Python/primeiraAula/exercicios.py
@@ -0,0 +1,92 @@
+#Exercicio 1 - Soma Simples
+'''
+numero1 = int(input('digite o primero numero: '))
+numero2 = int(input('digite o segundo numero: '))
+
+reultado = numero1 + numero2
+
+print(reultado);
+'''
+
+#Exercicio 2 - Area Retangulo
+'''
+largura = int(input('digite a largura: '))
+altura = int(input('digite a altura: '))
+
+area = largura * altura
+print('a area é:', area)
+'''
+
+#Exercicio 3 - Conversao idade para dias
+'''
+idade = int(input('Digite sua idade: '))
+dias_ano = 365
+
+idade_em_dias = idade * dias_ano
+print('O valor da sua idade em dias é ',idade_em_dias)
+'''
+
+#Exericio 4 - Divisao Inteira e Resto
+'''
+numero1 = int(input('digite o primeiro numero: '))
+numero2 = int(input('digite o segundo numero: '))
+
+divisao = numero1 / numero2
+restoDivisao = numero1 % numero2
+
+print('O valor da divisão é ',divisao)
+print('O valor do resto da divisão é ',restoDivisao)
+'''
+
+#Exercicio 5 - Calculadora de Gorjeta
+'''
+valor_conta = float(input('Digite o valor da conta: '))
+gorjeta = valor_conta * 0.1
+
+print('A gorjeta é ', gorjeta)
+'''
+
+#Exercicio 6 - Conversão de Temperatura
+'''
+temperatura_celsius = int(input('Digite a temperatura em celsius: '))
+conversao_farenheit = (temperatura_celsius * 1.8) + 32
+print('A temperatura em farenheit é ',conversao_farenheit)
+'''
+
+#Exercicio 7 - Quadrado e Raiz
+'''
+numero = int(input('Digite um numero: '))
+quadrado_numero = numero ** 2
+raiz_quadrada_numero = numero ** 0.5
+
+print('O quadrado do número é ', quadrado_numero)
+print('A raiz quadrada dess número é ', raiz_quadrada_numero)
+'''
+
+#Exercicio 8 - Concatenação de Strings
+'''
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+nome_completo = nome + ' ' + sobrenome
+
+print(nome_completo)
+'''
+
+#Exercicio 9 -  Reajuste Salarial
+'''
+salario_atual = float(input('Digite seu salario atual: '))
+reajuste_salario = salario_atual * 0.15
+salario_com_aumento = salario_atual + reajuste_salario
+
+print(salario_com_aumento)
+'''
+
+#Exercicio 10 - Tempo de Viagem
+'''
+distancia_km = int(input('Digite a distancia em km: '))
+velocidade_km_h = int(input('Digite a velocidade em km/h: '))
+tempo_de_viagem = distancia_km / velocidade_km_h
+
+print(tempo_de_viagem)
+'''
+
--- Python/primeiraAula/main.py
+++ Python/primeiraAula/main.py
@@ -1,4 +1,18 @@
-numero1 = 1
-numero2 = 2
+print('the quick  brown fox jumps over the laz dog');
+print('O valor de pi é ', 3.1415)
 
-print(numero1 + numero2)
+pi = 3.1415
+text = 'Bom dia'
+complex_number = 1 + 3j
+print(type(pi))
+print(type(text))
+print(type(complex_number))
+
+s = input('Digite uma palavra: ')
+print(s)
+
+c =  3 + 4j
+print('c = ', c)
+print('Parte real: ', c.real)
+print('Parte imaginaria: ', c.imag )
+print('Conjugado: ', c.conjugate())
