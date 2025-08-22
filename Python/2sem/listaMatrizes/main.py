c6e2f294177522784369b7224cd22289316e42f7
nnibo <nbaradel4@gmail.com>
2025-08-22 18:46:26 -0300

    teste

--- Python/2sem/listaMatrizes/main.py
+++ Python/2sem/listaMatrizes/main.py
@@ -70,48 +70,58 @@
 # ------------------------------------------------
 
 # EX 4
+# from random import randint
+# def criar_matriz_aleatoria(m, n):
+#     matriz = []
+#     for i in range(m):
+#         linha = []
+#         for j in range(n):
+#             linha.append(randint(1,10))
+#         matriz.append(linha)
+#     return matriz
+
+# def transposta(matriz):
+#     m = len(matriz)
+#     n = len(matriz[0])
+#     matriz_transposta = []
+#     for linha in range(n):
+#         matriz_transposta.append([])
+#         for coluna in range(m):
+#             matriz_transposta[coluna].append(matriz[linha][coluna])
+#     return matriz_transposta
+
+# matriz = criar_matriz_aleatoria(3,2)
+# matriz_tranposta = transposta(matriz)
+
+# for linha in matriz:
+#     print(linha)
+# ------------------------------------------------
+
+# EX 6
 from random import randint
-def criar_matriz_aleatoria(m, n):
+
+def criar_matriz(m, n):
     matriz = []
     for i in range(m):
         linha = []
         for j in range(n):
             linha.append(randint(1,10))
         matriz.append(linha)
+        # for elemento in linha:
+        #     elemento = elemento * 2
+        #     print(elemento)
+
     return matriz
 
-def transposta(matriz):
-    m = len(matriz)
-    n = len(matriz[0])
-    matriz_transposta = []
-    for linha in range(n):
-        matriz_transposta.append([])
-        for coluna in range(m):
-            matriz_transposta[coluna].append(matriz[linha][coluna])
-    return matriz_transposta
+def multiplicar_elementos_matriz(matriz, numero_multiplicacao):
+    matriz_multiplicada = criar_matriz(3,3)
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            matriz_multiplicada[i][j] = matriz[i][j] * numero_multiplicacao
+    return matriz_multiplicada          
 
-matriz = criar_matriz_aleatoria(3,2)
-matriz_tranposta = transposta(matriz)
+matriz = criar_matriz(3,3)
+print(matriz)
+print(multiplicar_elementos_matriz(matriz, 2))
 
-for linha in matriz:
-    print(linha)
-# ------------------------------------------------
-
-# EX 6
-# from random import randint
-#
-# def criar_matriz(m, n):
-#     matriz = []
-#     for i in range(m):
-#         linha = []
-#         for j in range(n):
-#             linha.append(random.randint(1,10))
-#         matriz.append(linha)
-#         for elemento in linha:
-#             elemento = elemento * 2
-#             print(elemento)
-#
-#     return matriz
-#
-# print(criar_matriz(3,3))
 
