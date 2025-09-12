970c96600ca6e5395371ff77764ccb33aaa176f9
nnibo <nbaradel4@gmail.com>
2025-09-11 23:49:33 -0300

    next app de estudo criado

---
+++ WebDev/2sem/estudo-cp4/app/layout.js
@@ -0,0 +1,11 @@
+import "./global.css"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ WebDev/2sem/estudo-cp4/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    // Exercicio 1
+    // <h1>Hello World</h1>
+
+    // ------------------------------------------------------------------
+
+    // Exercicio 2
+    
+  );
+}
